c0d52ace4e6cc4d1b5a7bfacf0aba4f8835a78df
ankit <ankitkumar897955@gmail.com>
2024-01-30 15:32:11 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,80 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import Input from './component/input'
-import TextField from '@mui/material/TextField';
+import React, { useEffect, useState } from "react";
+import Input from "./component/input";
 
 
-import './App.css'
+import "./App.css";
 
-const per_page=100;
+const per_page = 100;
 function App() {
-    const [images, setImages] = useState([])
-    const [search, setSearch] = useState('')
-    const [query, setQuery] = useState('office')
+  const [images, setImages] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("office");
 
-    const getAPI = `https://api.unsplash.com/search/photos?page=3&per_page=${per_page}&query=${query}`
+  const getAPI = `https://api.unsplash.com/search/photos?page=3&per_page=${per_page}&query=${query}`;
 
-    async function fetchAPI() {
-        const api = await fetch(getAPI, {
-            headers: {
-                Authorization: "Client-ID GXoqnmfqbXLTTuAipAlSeGbzdMomNuSvgBRwgSzqUII"
-            }
-        })
-        const data = await api.json()
-        console.log(data.results)
-        setImages(data.results)
-    }
-    useEffect(() =>{
-        fetchAPI()
+  async function fetchAPI() {
+    const api = await fetch(getAPI, {
+      headers: {
+        Authorization: "Client-ID GXoqnmfqbXLTTuAipAlSeGbzdMomNuSvgBRwgSzqUII",
+      },
+    });
+    const data = await api.json();
+    console.log(data.results);
+    setImages(data.results);
+  }
+  useEffect(() => {
+    fetchAPI();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[query])
+  }, [query]);
 
-    function handleChange(event) {
-        setSearch(event.target.value)
-        event.preventDefault()
+  function handleChange(event) {
+    setSearch(event.target.value);
+    event.preventDefault();
+  }
 
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    setQuery(search);
+    setSearch("");
+  }
 
-    function handleSubmit(event){
-        event.preventDefault()
-        setQuery(search)
-        setSearch('')
-    }
-   
-
-    return(
-        <>
-            <div >
-                <form onSubmit = {handleSubmit}>
-                <div className='textField'>
-
-                <TextField
-                   id="outlined-basic" label="Search Image" variant="outlined" 
-                    type = "text" 
-          
-                    onChange = {handleChange} 
-                    value = {search} 
-            
-                 
-                    inputProps = {{
-                        style: {
-                            color: 'red'
-                        }
-                    }}
-                    />
-                    </div>
-                </form>
-            </div>
-            <div className = "image-container">
-            {images.map((image) => {
-               return(
-                   <Input 
-                    key = {image.id}
-                    {...image}
-                   />
-               )
-           })}
-           </div>
-        </>
-    )
+  return (
+    <>
+      <div className="input_section">
+        <form onSubmit={handleSubmit}>
+          <div className="textField">
+            <input
+              type="text"
+              placeholder="Search something.."
+              onChange={handleChange}
+              value={search}
+             className="Search_box"
+            />
+            <button className="SeachBtn">Search</button>
+          </div>
+        </form>
+      </div>
+      <div className="image-container">
+        {images.map((image) => {
+          return <Input key={image.id} {...image} />;
+        })}
+      </div>
+    </>
+  );
 }
-export default App
+export default App;
